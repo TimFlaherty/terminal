411cf0f532b95ba919f33a6db758cf45d142c321
Tim Flaherty <tflahertytmf@gmail.com>
2017-05-25 19:45:56 -0700

    Patch vulnerable SQL injection validator

--- lib/val.php
+++ lib/val.php
@@ -2,7 +2,7 @@
 // Validation function to prevent SQL injection; 
 // Use: $foo = noInject($foo);
 	function noInject($str) {
-		$bad = array(";", "'", " or ", " Or ", " OR ", " oR ", ")", "(");
+		$bad = array(";", "*", "'", " or ", " Or ", " OR ", " oR ", ")", "(");
 		$good = str_replace($bad, "", $str);
 		return $good;
 	}
